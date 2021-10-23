eb4b7dd9841bc26c75ea7b671e9e3813bbd64e04
sajna-technohub <sajna@3gtechnohub.com>
2021-10-23 15:41:43 +0530

    commit_till_commercereport_updation

--- app/src/main/java/com/technohub/melifeapp/services/ApiClient.java
+++ app/src/main/java/com/technohub/melifeapp/services/ApiClient.java
@@ -11,8 +11,8 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "https://www.3gmelife.com:443/appmelife/webservice/Webservice/";
-    public static final String IMG_URL = "https://www.3gmelife.com:443/";
+    private static final String BASE_URL = "https://www.3gmelife.com/appmelife_alpha/webservice/Webservice/";
+    public static final String IMG_URL = "https://www.3gmelife.com/appmelife_alpha/";
     private static Retrofit retrofit;
     public static Retrofit getApiClient() {
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
