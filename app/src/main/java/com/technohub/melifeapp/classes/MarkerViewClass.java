d2ae03e12559a3bd0c5816f89b6ab3e4ab4b022b
sajna-technohub <sajna@3gtechnohub.com>
2021-10-25 17:58:37 +0530

    commit_till_streamreport_updation

---
+++ app/src/main/java/com/technohub/melifeapp/classes/MarkerViewClass.java
@@ -0,0 +1,47 @@
+package com.technohub.melifeapp.classes;
+
+import android.content.Context;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.components.MarkerView;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.utils.MPPointF;
+import com.technohub.melifeapp.R;
+
+public class MarkerViewClass extends MarkerView {
+
+    private TextView tvContent;
+
+    public MarkerViewClass(Context context, int layoutResource) {
+        super(context, layoutResource);
+
+        // find your layout components
+        tvContent = (TextView) findViewById(R.id.tvContent);
+    }
+
+    // callbacks everytime the MarkerView is redrawn, can be used to update the
+// content (user-interface)
+    @Override
+    public void refreshContent(Entry e, Highlight highlight) {
+
+        tvContent.setText("" + e.getY()+e.getX());
+//        tvContent.setText("Time : "+e.getY() + "  " + "Question No : "+e.getX());
+
+        // this will perform necessary layouting
+        super.refreshContent(e, highlight);
+    }
+
+    private MPPointF mOffset;
+
+    @Override
+    public MPPointF getOffset() {
+
+        if(mOffset == null) {
+            // center the marker horizontally and vertically
+            mOffset = new MPPointF(-(getWidth() / 2), -getHeight());
+        }
+
+        return mOffset;
+    }
+}
