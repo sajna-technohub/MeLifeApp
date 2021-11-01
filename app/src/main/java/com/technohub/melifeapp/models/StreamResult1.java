a0ff48972d34c1bde80a1cb6603d769301228dd6
sajna-technohub <sajna@3gtechnohub.com>
2021-11-01 18:43:02 +0530

    commit_till_allrepexceptright_updation

---
+++ app/src/main/java/com/technohub/melifeapp/models/StreamResult1.java
@@ -0,0 +1,77 @@
+package com.technohub.melifeapp.models;
+
+public class StreamResult1 {
+    public String sub_dimension_id;
+    public String dimension_id;
+    public String dimension;
+    public String sub_dim_order_id;
+    public String detail_description;
+    public String brief_description;
+    public String icon;
+
+    public String getSub_dimension_id() {
+        return sub_dimension_id;
+    }
+
+    public void setSub_dimension_id(String sub_dimension_id) {
+        this.sub_dimension_id = sub_dimension_id;
+    }
+
+    public String getDimension_id() {
+        return dimension_id;
+    }
+
+    public void setDimension_id(String dimension_id) {
+        this.dimension_id = dimension_id;
+    }
+
+    public String getDimension() {
+        return dimension;
+    }
+
+    public void setDimension(String dimension) {
+        this.dimension = dimension;
+    }
+
+    public String getSub_dim_order_id() {
+        return sub_dim_order_id;
+    }
+
+    public void setSub_dim_order_id(String sub_dim_order_id) {
+        this.sub_dim_order_id = sub_dim_order_id;
+    }
+
+    public String getDetail_description() {
+        return detail_description;
+    }
+
+    public void setDetail_description(String detail_description) {
+        this.detail_description = detail_description;
+    }
+
+    public String getBrief_description() {
+        return brief_description;
+    }
+
+    public void setBrief_description(String brief_description) {
+        this.brief_description = brief_description;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getSub_dimension() {
+        return sub_dimension;
+    }
+
+    public void setSub_dimension(String sub_dimension) {
+        this.sub_dimension = sub_dimension;
+    }
+
+    public String sub_dimension;
+}
